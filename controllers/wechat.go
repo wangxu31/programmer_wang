9a51838427b78c2122e09621190f2e22235b3630
wangxu31 <874760401@qq.com>
2018-11-10 13:52:54 +0800

    add

--- controllers/wechat.go
+++ controllers/wechat.go
@@ -71,7 +71,16 @@ func (c *WeChatController) Answer() {
 	log.Println(info)
 	//c.Ctx.WriteString(info.Content)
 	//c.Ctx.WriteString(string(c.Ctx.Input.RequestBody))
-	c.Data["xml"] = "success"
+	c.Data["xml"] = `<xml>
+	   <ToUserName><![CDATA[o9O7PwKec9bZRRejpiyFlAL_Sgwk]]></ToUserName>
+	   <FromUserName><![CDATA[gh_bacfcce806e4]]></FromUserName>
+	   <CreateTime>1541764082</CreateTime>
+	   <MsgType><![CDATA[text]]></MsgType>
+	   <Content><![CDATA[hhh]]></Content>
+	   <MsgId>6621826310771202218</MsgId>
+	   <Encrypt><![CDATA[n/ZdEQL/mcsR/ZeQVjuyqeX9fnaqWbS9zKTAt5b9+/TCv3Vo/l882Cjruji5gcQfsz3SJvLT/7+UT7rwu/neqIstGxox7pPYjKF6GFi3EZXyL+hQHuNH5kATiy6Xe/cH/Mqko8BdhKdB9RdT/NcBrGT1oWqkOeWNOLlfzVhHZxQZd3Yc2pvAg1UnLZxm1DX+V6Sfqud/s+kqLlzbpjc4v3O3EOhx/ja8ZtAXK4LipJ2Mt4aa3fjxpxYy1JSb1Dn+st1K2alw/qBX4TenvQpn+GBdDrCWnLDpSAM3ZDL+HtUDj4zAK5Tlun8YDJQKdQg884tbqVQgAvG2dVhyY4UAYacFZxP3+AhCrrA3CkiaqVPIjSMBGZVMsM9B3mNfql/hYKsx4ELhwN27PUdpNm+2s9+vj1MGl7tqXB8QGXIaj3A=]]></Encrypt>
+	</xml>`
+
 	c.ServeXML()
 }
 
